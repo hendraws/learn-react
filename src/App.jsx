98629087be29c6d87cb87e8ae4071edbd3fd164d
hendraws <saputra.hendrawijaya@gmail.com>
2023-01-03 11:32:29 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-600 grid place-content-center min-h-screen ">
+      <h1 className="text-xl">Learn React</h1>
+    </div>
+  );
+};
+
+export default App;
